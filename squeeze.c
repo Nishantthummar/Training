f53bdb044b8769bde9e4f0859a5a74d2607ad6c9
“Nishanthummar” <nishant.thummar@softnautics.com>
2020-01-09 19:07:17 +0530

    assignment-2

--- squeeze.c
+++ squeeze.c
@@ -1,38 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
+void squeeze();
 void main()
 {
-	char s1[10],s2[10];
-	char i,j,count=10,k;
-	printf("\nEnter the s1 ");
-	for(i=0;i<10;i++)
-	{
-		scanf("%c",&s1[i]);
-	}
-	printf("\nEnter the s2");
-	for(i=0;i<10;i++)
-	{
-		scanf("%c",&s2[i]);
-	}
-	for(i=0;i<10;i++)
-	{
-		for(j=i+1;j<10;j++)
-		{
-			if(s1[i]==s2[j])
-			{
-				for(k=i;k<10-i;k++)
-				{
-				s1[i]=s1[i+1];
-				//count--;
-				printf("count =%d",count);
+	//int i;
+	char s1[10],s2[10]; /*< Two array */
+	//p1 = &s1[0]; /*< pointer of first array */
+	//p2 = &s2[0]; /*< pointer of second array */
+	printf("enter the one");
+	scanf("%s", &s1[0]);
+	printf("\nsecond ");
+	scanf("%s", &s2[0]);	
+	squeeze(s1,s2);
+	printf("\nnew s1 is ");
+	printf(" %s", s1);
+}
+void squeeze(char p1[], char p2[])
+{
+	int i,j,k;
+	printf("\ninside p1 %s", p1);
+	printf("\ninside p2 %s", p2);
+	for (i=0; i<10; i++) {
+		for (j=0; j<10; j++) {
+			if (p1[i] == p2[j]) {
+				for( k=i; k<10; k++) {
+					p1[k] = p1[k+1];
+					//printf(" p1 %c", p1[i]);
 				}
-				count--;
+				p1[k-1] = '\0';
 			}
 		}
-	}	
-	printf("\nnew str is");
-	for(i=0;i<10-count;i++)
-	{
-		printf("%c",s1[i]);
-	
 	}
 }
+
+
+
+	
+
