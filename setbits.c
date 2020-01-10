1274eacb75baa2dc6029b93a90867671195cbfa1
“Nishanthummar” <nishant.thummar@softnautics.com>
2020-01-10 10:18:31 +0530

    assignment-2

--- setbits.c
+++ setbits.c
@@ -1,44 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
+void setbits();
 void main()
 {
-	int a1[10],a2[10];
-	int i,j,no,temp,p;
+	int a1[10], a2[10];
+	int i, j, no, temp, p;
 	printf("\nenter the string with 10 elemnets");
-	for(i=0;i<10;i++)
-	{
-		scanf("%d",&a2[i]);
+	for (i=0; i<10; i++) {
+		scanf("%d", &a2[i]);
 	}
 	printf("\nYou enter");
-	for (i=0;i<10;i++)
-	{
-		printf("\n%d",a2[i]);
+	for (i=0; i<10; i++) {
+		printf("\n%d", a2[i]);
 	
 	}
 	printf("enter the no of bits");
-	scanf("%d",&no);
+	scanf("%d", &no);
 	printf("enter the position");
-	scanf("%d",&p);
-	//printf("\nposition is %d",p);
-	printf("new series is\n");
-	j=0;
-	//printf("sum is%d",p+no);	
-	if(p+no<=10)
-	{
-		for(i=p;i<p+no;i++)
-		{
-			//printf(" \ni is%d , j is %d",i,j);		
-			a1[j]=a2[i];
-			j++;
-			//printf("\n%d",a1[j]);
-		}
-		for(i=0;i<no;i++)
-		{
-			printf("\n%d",a1[i]);
-		}
+	scanf("%d", &p);
+	setbits(a1, p, no, a2);
+	for (i=0; i<=10-(p+no); i++) {
+		printf("\na[%d] is %d", i, a1[i]);
 	}
-	else
-	{
-		printf("\nInvalid you enter");
+}
+void setbits(int s1[], int p, int n, int s2[])
+{
+	int i, k, j = 0;
+	for (i=p; i<p+n; i++) {
+		s1[j] = s2[i];
+		j++;
 	}
 }
 
